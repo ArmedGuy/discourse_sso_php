ad1da304c4f4d9c673158a489d51445eff393f94
Johan Jatko <johan@pie-studios.com>
2014-02-26 21:20:30 +0100

    Rewritten to be more clear

--- discourse_sso.php
+++ discourse_sso.php
@@ -1,7 +1,7 @@
 <?php
 class Discourse_SSO {
+
 	private $sso_secret;
-	private $nonce;
 	
 	function __construct($secret) {
 		$this->sso_secret = $secret;
@@ -10,23 +10,30 @@ class Discourse_SSO {
 	public function validate($payload, $sig) {
 		$payload = urldecode($payload);
 		if(hash_hmac("sha256", $payload, $this->sso_secret) === $sig) {
-			$query = array();
-			parse_str(base64_decode($payload), $query);
-			
-			$this->nonce = $query["nonce"];
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public function loginstring($params) {
+	public function getNonce($payload) {
+		$payload = urldecode($payload);
+		$query = array();
+		parse_str(base64_decode($payload), $query);
+		if(isset($query["nonce")) {
+			return $query["nonce"];
+		} else {
+			throw new Exception("Nonce not found in payload!");
+		}
+	}
+	public function buildLoginString($params) {
 		if(!isset($params["external_id"])) {
 			throw new Exception("Missing required parameter 'external_id'");
 		}
-		$params['nonce'] = $this->nonce;
-		$payload = http_build_query($params);
-		$payload = base64_encode($payload);
+		if(!isset($params["nonce"])) {
+			throw new Exception("Missing required parameter 'nonce'");
+		}
+		$payload = base64_encode(http_build_query($params));
 		$sig = hash_hmac("sha256", $payload, $this->sso_secret);
 		
 		return http_build_query(array("sso" => $payload, "sig" => $sig));
