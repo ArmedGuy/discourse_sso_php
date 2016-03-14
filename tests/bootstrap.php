d5812d62a685287f149d11301f5e70968aea9ebf
Derek Illchuk <dillchuk@gmail.com>
2016-03-13 21:06:29 -0500

    PHPUnit testing, Travis CI setup + $extraParameters should not have the ability to overwrite given nonce/email/external_id.

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+foreach (['.', '..', '../../..'] as $dir) {
+    $autoload = "{$dir}/vendor/autoload.php";
+    if (file_exists($autoload)) {
+        include $autoload;
+        return;
+    }
+}
+throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
